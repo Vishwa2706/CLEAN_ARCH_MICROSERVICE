d382466426c2ca02d9896cc33342ba4be3ad469a
Vishwa2706 <vishwajayanth3@gmail.com>
2026-02-03 12:20:59 +0530

    Implemented authentication middleware

--- APIGateway/Program.cs
+++ APIGateway/Program.cs
@@ -1,16 +1,58 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+// Load configs
+builder
+    .Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
-// Add Ocelot
+// Read JWT values
+var jwtKey = builder.Configuration["Jwt:Key"];
+var jwtIssuer = builder.Configuration["Jwt:Issuer"];
+var jwtAudience = builder.Configuration["Jwt:Audience"];
+
+// Authentication
+builder
+    .Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.RequireHttpsMetadata = false;
+        options.SaveToken = true;
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            ValidIssuer = jwtIssuer,
+            ValidAudience = jwtAudience,
+
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey)),
+
+            ClockSkew = TimeSpan.Zero,
+        };
+    });
+
+// Authorization
+builder.Services.AddAuthorization();
+
+// Ocelot
 builder.Services.AddOcelot();
 
 var app = builder.Build();
 
-// Use Ocelot middleware
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 await app.UseOcelot();
 
 app.Run();
